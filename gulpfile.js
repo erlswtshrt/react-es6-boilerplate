02139fe8341068cb96c6db81da9a5856ffff9191
John Earle <johnearle@Johns-MacBook-Pro-2.local>
2015-12-27 10:46:20 -0800

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,27 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var gulpif = require('gulp-if');
-var streamify = require('gulp-streamify');
-var autoprefixer = require('gulp-autoprefixer');
-var cssmin = require('gulp-cssmin');
-var less = require('gulp-less');
-var concat = require('gulp-concat');
-var plumber = require('gulp-plumber');
-var source = require('vinyl-source-stream');
-var babelify = require('babelify');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var uglify = require('gulp-uglify');
+var gulp          = require('gulp');
+var gutil         = require('gulp-util');
+var gulpif        = require('gulp-if');
+var streamify     = require('gulp-streamify');
+var autoprefixer  = require('gulp-autoprefixer');
+var cssmin        = require('gulp-cssmin');
+var sass          = require('gulp-sass');
+var plumber       = require('gulp-plumber');
+var source        = require('vinyl-source-stream');
+var babelify      = require('babelify');
+var browserify    = require('browserify');
+var watchify      = require('watchify');
+var uglify        = require('gulp-uglify');
 
 var production = process.env.NODE_ENV === 'production';
 
 var dependencies = [
-  'alt',
   'react',
   'react-dom',
-  'react-router',
-  'underscore'
+  'react-router'
 ];
 
-/*
- |--------------------------------------------------------------------------
- | Combine all JS libraries into a single file for fewer HTTP requests.
- |--------------------------------------------------------------------------
- */
-gulp.task('vendor', function() {
-  return gulp.src([
-    'bower_components/jquery/dist/jquery.js',
-    'bower_components/bootstrap/dist/js/bootstrap.js',
-    'bower_components/magnific-popup/dist/jquery.magnific-popup.js',
-    'bower_components/toastr/toastr.js'
-  ]).pipe(concat('vendor.js'))
-    .pipe(gulpif(production, uglify({ mangle: false })))
-    .pipe(gulp.dest('public/js'));
-});
-
-/*
- |--------------------------------------------------------------------------
- | Compile third-party dependencies separately for faster performance.
- |--------------------------------------------------------------------------
- */
-gulp.task('browserify-vendor', function() {
-  return browserify()
-    .require(dependencies)
-    .bundle()
-    .pipe(source('vendor.bundle.js'))
-    .pipe(gulpif(production, streamify(uglify({ mangle: false }))))
-    .pipe(gulp.dest('public/js'));
-});
-
-/*
- |--------------------------------------------------------------------------
- | Compile only project files, excluding all third-party dependencies.
- |--------------------------------------------------------------------------
- */
-gulp.task('browserify', ['browserify-vendor'], function() {
+// bundle once
+gulp.task('browserify', function() {
   return browserify('app/main.js')
     .external(dependencies)
     .transform(babelify, { presets: ['es2015', 'react'] })
@@ -68,12 +31,8 @@ gulp.task('browserify', ['browserify-vendor'], function() {
     .pipe(gulp.dest('public/js'));
 });
 
-/*
- |--------------------------------------------------------------------------
- | Same as browserify task, but will also watch for changes and re-compile.
- |--------------------------------------------------------------------------
- */
-gulp.task('browserify-watch', ['browserify-vendor'], function() {
+// watch and bundle on change
+gulp.task('browserify-watch', function() {
   var bundler = watchify(browserify('app/main.js', watchify.args));
   bundler.external(dependencies);
   bundler.transform(babelify, { presets: ['es2015', 'react'] });
@@ -94,23 +53,20 @@ gulp.task('browserify-watch', ['browserify-vendor'], function() {
   }
 });
 
-/*
- |--------------------------------------------------------------------------
- | Compile LESS stylesheets.
- |--------------------------------------------------------------------------
- */
+// compile to css
 gulp.task('styles', function() {
-  return gulp.src('app/stylesheets/main.less')
+  return gulp.src('app/stylesheets/main.scss')
     .pipe(plumber())
-    .pipe(less())
+    .pipe(sass())
     .pipe(autoprefixer())
     .pipe(gulpif(production, cssmin()))
     .pipe(gulp.dest('public/css'));
 });
 
+// recompile on change
 gulp.task('watch', function() {
-  gulp.watch('app/stylesheets/**/*.less', ['styles']);
+  gulp.watch('app/stylesheets/**/*.scss', ['styles']);
 });
 
-gulp.task('default', ['styles', 'vendor', 'browserify-watch', 'watch']);
-gulp.task('build', ['styles', 'vendor', 'browserify']);
+gulp.task('default', ['browserify-watch','watch']);
+gulp.task('build', ['styles', 'browserify']);
